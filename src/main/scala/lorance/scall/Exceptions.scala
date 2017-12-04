cd27025b64645fc202414b7339fa82b2f59f390d
LoranceChen <w68055010@qq.com>
2017-12-05 00:50:05 +0800

    remove dead code

--- src/main/scala/lorance/scall/Exceptions.scala
+++ src/main/scala/lorance/scall/Exceptions.scala
@@ -6,14 +6,7 @@ package lorance.scall
 class ScallException(msg: String) extends RuntimeException(msg)
 
 case class ExcFailException(error: Error) extends ScallException(error.toString)
-//case class ExcBatchFailException(error: Error, cmd: Cmd, index: Int) extends
-//  ScallException(s"${index}st command: $cmd, execute fail: $error")
-//
-//case object ExitRootShell extends ScallException("root Shell env can't exit, use disconnect if you want close")
 case class StreamClosedException(msg: String) extends ScallException(msg)
-//case class ShellContextException(msg: String) extends ScallException(msg)
-//case class NetworkDisconnect(shell: Shell) extends ScallException(s"Shell back to level:${shell.levelId}")
-//case object ShellLevelFlow extends ScallException("can NOT setting sub shell more then 256")
 
 
 //Terminal exception
