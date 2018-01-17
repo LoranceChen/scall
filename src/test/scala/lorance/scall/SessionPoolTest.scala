8e1c91d8dd8c2516ae36b469b43e7827aa06b330
LoranceChen <w68055010@qq.com>
2018-01-17 12:55:37 +0800

    refine test comment

--- src/test/scala/lorance/scall/SessionPoolTest.scala
+++ src/test/scala/lorance/scall/SessionPoolTest.scala
@@ -28,20 +28,20 @@ class SessionPoolTest {
   @Test
   def onMultiThread(): Unit = {
 
-    val sessionPool = new SessionPool(Auth("localhost", None, 22, Password("xxx")), Config(10, 5, 2), 10, 1000)
+    val sessionPool = new SessionPool(Auth("localhost", None, 22, Password(" ")), Config(10, 5, 2), 20, 1000)
     def parTest(count: Int) = {
-      println("begin - " + System.currentTimeMillis())
+      println("begin milli time - " + System.currentTimeMillis())
       (1 to count).toList.map { index =>
-        val rst = sessionPool.execAsync(Cmd("sleep 30;cd ~/tmp;pwd"))
-        rst.foreach(x => println(s"end - $index - ${System.currentTimeMillis()} - $x"))
+        val rst = sessionPool.execAsync(Cmd("cd ~/tmp;pwd"))
+        rst.foreach(x => println(s"end milli time - $index - ${System.currentTimeMillis()} - $x"))
         rst
       }
     }
 
     //max session MaxSession / 2
-    Await.result(Future.sequence(parTest(10)), 100 seconds)
+    Await.result(Future.sequence(parTest(300)), 100 seconds)
 
-    Thread.sleep(10 * 1000)
-    Await.result(Future.sequence(parTest(200)), 600 seconds)
+    Thread.sleep(6 * 1000)
+    Await.result(Future.sequence(parTest(3000)), 600 seconds)
   }
 }
