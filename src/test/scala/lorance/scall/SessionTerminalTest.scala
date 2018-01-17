67db2653cc7e46ed3a130a5d5f197efec5aac718
LoranceChen <w68055010@qq.com>
2018-01-17 11:48:22 +0800

    comment jsch session relative class

--- src/test/scala/lorance/scall/SessionTerminalTest.scala
+++ src/test/scala/lorance/scall/SessionTerminalTest.scala
@@ -12,25 +12,4 @@ class SessionTerminalTest {
     println(sessionTerminal.exc(Cmd("""pwd""")))
   }
 
-  @Test
-  def simpleUsage2(): Unit = {
-//    val sessionTerminal = new SessionTerminal2(Auth("localhost", None, 22, Password(" ")), Config(10, 5, 2))
-//
-//    println(sessionTerminal.exc(Cmd("""cd ./not-exist-dir && pwd""")))
-//    println(sessionTerminal.exc(Cmd("""pwd""")))
-  }
-
-  @Test
-  def simpleUsage3(): Unit = {
-    import scala.concurrent.ExecutionContext.Implicits.global
-    val sessionTerminal = new SessionTerminalPool(Auth("localhost", None, 22, Password(" ")), Config(10, 5, 2), 20)
-
-    (1 to 100).foreach(_ =>
-      new Thread(() => {
-        sessionTerminal.excAsync(Cmd("""cd ./not-exist-dir && pwd""")).foreach(println)
-        sessionTerminal.excAsync(Cmd("""cd ~/tmp;pwd""")).foreach(println)
-      }).start())
-
-    Thread.currentThread().join()
-  }
 }
